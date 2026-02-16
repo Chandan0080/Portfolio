7db96a5e31e2d566d1eb74de3474ef954778ddac
Chandan Gupta <74350643+Chandan0080@users.noreply.github.com>
2026-02-17 01:43:29 +0530

    Update CV download link in HeroSection component

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -49,7 +49,7 @@ const HeroSection = () => {
                                 Contact Me
                         </button>
                     </a>
-                    <a href="/CHANDAN_CV.pdf" download="cv" className="flex flex-col  justify-center gap-4">
+                    <a href="/Chandan_s_CV.pdf" download="cv" className="flex flex-col  justify-center gap-4">
                         <button  class="bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500 hover:from-pink-500 hover:to-purple-600 text-white font-bold py-3 px-6 rounded-full shadow-lg transform transition-all duration-500 ease-in-out hover:scale-110 hover:brightness-110 hover:animate-pulse active:animate-bounce">
                                 Download CV
                         </button>                       
